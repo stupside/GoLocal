705b84efed824fea0cd8d74f083cafe631dce094
Ilyesbouty <ilyesbouty@icloud.com>
2021-05-31 22:27:36 +0200

    Added Tests for Shop

--- GoLocal.Core.Artisan.IntegrationTests/Commands/Shop/CreateShopTests.cs
+++ GoLocal.Core.Artisan.IntegrationTests/Commands/Shop/CreateShopTests.cs
@@ -1,7 +1,10 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
+using GoLocal.Bus.Results.Enums;
 using GoLocal.Core.Artisan.Application.Commands.Shops.CreateShop;
 using GoLocal.Core.Artisan.Application.Commands.Shops.CreateShop.Models;
 using GoLocal.Core.Artisan.IntegrationTests.Commons;
+using FluentAssertions;
 using NUnit.Framework;
 
 namespace GoLocal.Core.Artisan.IntegrationTests.Commands.Shop
@@ -11,29 +14,54 @@ namespace GoLocal.Core.Artisan.IntegrationTests.Commands.Shop
         public CreateShopTests()
         {
         }
-        
+
         [Test]
-        public async Task CreateShopTest()
+        public async Task CreateShopWithoutLocation_Test()
         {
             var result = await Mediator.Send(new CreateShopCommand
             {
                 Name = "Test",
                 Location = new LocationDto
                 {
-                    Street = "Doyen Gosse",
-                    PostCode = "38000",
-                    Country = "France",
-                    City = "Grenoble",
-                    Address = "2",
-                    Region = "Rhone Alpes"
+                    Street = null,
+                    PostCode = null,
+                    Country = null,
+                    City = null,
+                    Address = null,
+                    Region = null
                 },
-                
+
                 Contact = new ContactDto
                 {
                     Phone = "test",
                     Email = "test@test.com"
                 }
             });
+            result.Status.Should().Be(ResultStatus.BadRequest);
+        }
+
+        [Test]
+        public async Task CreateShopWithEmailInvalid_Test()
+        {
+            throw new NotImplementedException();
+        }
+        
+        [Test]
+        public async Task CreateShopWithEmailValid_Test()
+        {
+            throw new NotImplementedException();
+        }
+        
+        [Test]
+        public async Task CreateShopWithLocationInvalid_Test()
+        {
+            throw new NotImplementedException();
+        }
+        
+        [Test]
+        public async Task CreateShopWithLocationValid_Test()
+        {
+            throw new NotImplementedException();
         }
     }
 }
