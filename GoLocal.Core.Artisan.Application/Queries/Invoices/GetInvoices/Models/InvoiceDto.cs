0bef952a8d942cde219fcdd06d2e5a1facddc317
stupside <kilian.houpeurt@gmail.com>
2021-06-01 20:15:50 +0200

    Fixed empty InvoiceDto

--- GoLocal.Core.Artisan.Application/Queries/Invoices/GetInvoices/Models/InvoiceDto.cs
+++ GoLocal.Core.Artisan.Application/Queries/Invoices/GetInvoices/Models/InvoiceDto.cs
@@ -1,7 +1,17 @@
-﻿namespace GoLocal.Core.Artisan.Application.Queries.Invoices.GetInvoices.Models
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using GoLocal.Core.Domain.Enums;
+
+namespace GoLocal.Core.Artisan.Application.Queries.Invoices.GetInvoices.Models
 {
     public class InvoiceDto
     {
+        public int Id { get; init; }
+        public InvoiceStatus Status { get; init; }
+        public IEnumerable<InvoiceItemDto> InvoiceItems { get; init; }
+        public float TotalPrice => InvoiceItems.Sum(m => m.Price * m.Quantity);
         
+        public DateTime Creation { get; init; }
     }
 }
